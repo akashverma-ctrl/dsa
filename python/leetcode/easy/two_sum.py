928e38e57676ed117b33fadd2e8fecd4c6d12eb2
akashverma-ctrl <akashverma311099@gmail.com>
2026-04-05 10:48:18 +0530

    Add test case to two sum problem and add gitignore file.

--- python/leetcode/easy/two_sum.py
+++ python/leetcode/easy/two_sum.py
@@ -1,42 +1,39 @@
 """
+Problem Statement
+
 1. Two Sum
-
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
- 
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
 
 Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
 Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
- 
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
 
 Constraints:
+    2 <= nums.length <= 104
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+    Only one valid answer exists.
 
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
- 
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 """
 
+import unittest
+
+
 class TwoSum:
-    def two_sum_dict(self, nums: list[int], target: int) -> list[int]:
+    def two_sum_dict(self, nums: list[int], target: int) -> list[int] | str:
         """
         This solution include a dict to store the mapping of number and it's index
         Time complexity: O(n)
@@ -48,10 +45,34 @@ class TwoSum:
             if rem in result:
                 return [result[rem], i]
             result[nums[i]] = i
+        return "Not Found"
+
+
+class TestTwoSum(unittest.TestCase):
+    def setUp(self):
+        self.two_sum = TwoSum()
+
+    def test_two_sum_dict_when_vaild_input_given__expect_valid_target(self):
+        nums = [2, 7, 11, 15]
+        target = 9
+        expected = [0, 1]
+        actual = self.two_sum.two_sum_dict(nums, target)
+        self.assertEqual(actual, expected)
+
+    def test_two_sum_dict_when_invaild_input_given__expect_not_found(self):
+        nums = [2, 6, 11, 15]
+        target = 9
+        expected = "Not Found"
+        actual = self.two_sum.two_sum_dict(nums, target)
+        self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
-    nums = [2,7,11,15]
-    target = 9
-    two_sum = TwoSum()
-    indexes = two_sum.two_sum_dict(nums, target)
-    print("Indices of the two numbers such that they add up to target are: ", indexes)
+    unittest.main()
+
+    # # Uncomment below to run code locally
+    # nums = [2,7,11,15]
+    # target = 9
+    # two_sum = TwoSum()
+    # indexes = two_sum.two_sum_dict(nums, target)
+    # print("Indices of the two numbers such that they add up to target are: ", indexes)
